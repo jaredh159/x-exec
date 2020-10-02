0089e305266a9987c212153286d7c42258eb0e7f
Jared Henderson <jared@netrivet.com>
2020-10-02 15:50:28 -0400

    add exec.out for passing thru stdout/err

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { execSync } from 'child_process';
+import { execSync, spawnSync } from 'child_process';
 
 export interface ErrData {
   exitCode: number;
@@ -23,6 +23,26 @@ export default function exec(cmd: string, cwd?: string): [null | ErrData, string
   }
 }
 
+/**
+ *
+ * Synchronously exec a command, sending all stdout/stderr
+ * straight through to the current process's stdout/stderr.
+ * Return a boolean representing the process exit code.
+ */
+exec.out = function (cmd: string, cwd?: string): boolean {
+  const parts = cmd.split(/ +/g);
+  const { status } = spawnSync(parts.shift(), parts, {
+    stdio: `inherit`,
+    ...(cwd ? { cwd } : {}),
+  });
+  return status === 0;
+};
+
+/**
+ * Run a command, returning the stdout as a string.
+ * If the command was unsuccessful (exit=1)
+ * log the error and immediately exit 1
+ */
 exec.exit = function (cmd: string, cwd?: string): string {
   const [err, msg] = exec(cmd, cwd);
   if (!err) {
@@ -33,6 +53,10 @@ exec.exit = function (cmd: string, cwd?: string): string {
   process.exit(1);
 };
 
+/**
+ * Run a command, swallowing all output, only returning
+ * a single boolean representing the exit status of the command.
+ */
 exec.success = function (cmd: string, cwd?: string): boolean {
   const [err] = exec(cmd, cwd);
   return !err;
